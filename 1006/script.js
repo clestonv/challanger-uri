b7a92818fb29db8b4af4e4ba9c1a00a0d48d2857
Cleberson Osorio <cleberson.osorio@hotmail.com>
2020-06-21 22:18:30 -0300

    challanger 06 finish

---
+++ 1006/script.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('stdin','utf8')
+
+var lines = input.split('\n')
+
+A = parseFloat(lines.shift())
+B = parseFloat(lines.shift())
+C = parseFloat(lines.shift())
+
+media = (A * 2 + B * 3 + C * 5) / 10
+
+console.log('MEDIA = '+media.toFixed(1))
